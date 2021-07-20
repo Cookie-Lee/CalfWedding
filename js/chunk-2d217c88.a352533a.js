8400ba414dcc416262487fc72511a6e0c3a4206f
Cookie-Lee <61486685+Cookie-Lee@users.noreply.github.com>
2021-07-20 10:40:02 +0800

    Updates

---
+++ js/chunk-2d217c88.a352533a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217c88"],{c7ca:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-fade-transition",{attrs:{mode:"out-in"}},[t("router-view")],1)},c=[],i={name:"BaseView"},r=i,o=t("2877"),s=t("6544"),u=t.n(s),l=t("0789"),w=Object(o["a"])(r,a,c,!1,null,null,null);e["default"]=w.exports;u()(w,{VFadeTransition:l["c"]})}}]);
+//# sourceMappingURL=chunk-2d217c88.a352533a.js.map
